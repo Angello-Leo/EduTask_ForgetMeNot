a3969f9344b05b76e899164398b569082336a296
Angello-Leo <gellovisto@gmail.com>
2025-11-25 18:06:20 +0800

    Renamed Files

--- Design/Design/obj/Debug/net8.0-windows/Design.AssemblyInfo.cs
+++ Design/Design/obj/Debug/net8.0-windows/Design.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Design")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e284bbc1810ac60cee42b715fd1a7097c5d725d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da01fcc7426f1324ac56d3bfb00ba021875687ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Design")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Design")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
