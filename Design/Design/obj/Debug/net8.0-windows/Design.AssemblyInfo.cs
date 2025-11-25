82fdf2d30d1c10dd8004d8e04e5872a31acaa095
Angello-Leo <gellovisto@gmail.com>
2025-11-25 17:07:51 +0800

    reomoved .vs

--- Design/Design/obj/Debug/net8.0-windows/Design.AssemblyInfo.cs
+++ Design/Design/obj/Debug/net8.0-windows/Design.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Design")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86dfcec6ced7b517213b6fd8542bed3990858c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e284bbc1810ac60cee42b715fd1a7097c5d725d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Design")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Design")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
